c73cfce247630f40c10417e2fc734fb53021ae4b
anrobin <50176492+anrobin719@users.noreply.github.com>
2019-06-03 19:01:15 +0900

    add random pj

---
+++ random/random-gradient.js
@@ -0,0 +1,17 @@
+var bgGradient = [];
+bgGradient[0] = "linear-gradient(140deg, #f37298, #fe9273)";
+bgGradient[1] = "linear-gradient(140deg, #509aaf, #73cbc1 50%, #f5ffc3)";
+bgGradient[2] = "linear-gradient(140deg, #f37298, #fe9273)";
+bgGradient[3] = "linear-gradient(140deg, #111111, #fe2273)";
+bgGradient[4] = "linear-gradient(140deg, #f32458, #f14273)";
+bgGradient[5] = "linear-gradient(140deg, #f32421, #fe3245)";
+bgGradient[6] = "linear-gradient(140deg, #f3ff38, #fe9273)";
+bgGradient[7] = "linear-gradient(140deg, #fa4598, #fe9273)";
+bgGradient[8] = "linear-gradient(140deg, #f37298, #a4573)";
+bgGradient[9] = "linear-gradient(140deg, #f35749, #fe9273)";
+
+var bg = document.body.style.backgroundImage;
+function color() {
+    
+}
+bg = bgGradient[index];
